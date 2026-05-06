eeb7252f9dc8c22c9c1a13ee1cbebe01426685db
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2026-05-05 21:56:10 -0300

    Initial Commit

---
+++ ArtGalery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGalery
+//
+//  Created by Livia Rosembach Oliveira on 05/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
